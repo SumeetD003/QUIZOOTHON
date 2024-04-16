f98b657ea3084c949c91aefa170e497b8a1888c0
Sumeet Deshpande <sumeetdeshpande003@gmail.com>
2024-04-16 17:09:22 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st. set_page_config(
+    page_title='QUIZOOTHON',
+    page_icon="📖",
+    layout='centered',
+    initial_sidebar_state='collapsed'
+)
+
+st.title('Welcome to QUIZOOTHON 🧠!')
+st.write('This is a fantastic quiz competition!')
