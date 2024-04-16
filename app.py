eefaf82b656aff3686b4303e1c1cf724b4bb8568
Sumeet Deshpande <sumeetdeshpande003@gmail.com>
2024-04-16 20:06:46 +0530

    update1app.py

--- app.py
+++ app.py
@@ -12,3 +12,23 @@ st. set_page_config(
 
 st.title('Welcome to QUIZOOTHON 🧠!')
 st.write('This is a fantastic quiz competition!')
+
+
+
+with st.sidebar:
+    st.header("👨‍💻 About the Author")
+    st.write("""
+    **Sumeet Deshpande** is a tech enthusiast, student, and coder. Driven by passion and a love for AI and ML, he's developed this platform to make learning more interactive and fun.
+
+    Connect, contribute, or just say hi!
+    """)
+
+    st.divider()
+    st.subheader("🔗 Connect with Me", anchor=False)
+    st.markdown(
+            """
+           
+            - [🌐 Personal Website](https://digital-cv-5tfl.onrender.com/)
+            - [👔 LinkedIn](https://www.linkedin.com/in/sumeet-deshpande-b33840227/)
+            """
+        )
